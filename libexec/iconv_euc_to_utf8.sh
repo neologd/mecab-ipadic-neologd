15f9597abd5847d91a47b72b2231bdbedbbabee7
Toshinori Sato (@overlast) <overlasting@gmail.com>
2015-03-09 19:36:37 +0900

    Add license

--- libexec/iconv_euc_to_utf8.sh
+++ libexec/iconv_euc_to_utf8.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
+# Copyright (C) 2015 Toshinori Sato (@overlast)
+#
+#       https://github.com/neologd/mecab-ipadic-neologd
+#
+# Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 FILE_NAME=$1
 iconv -f EUC-JP -t UTF-8 ${FILE_NAME} > ${FILE_NAME}.utf8
