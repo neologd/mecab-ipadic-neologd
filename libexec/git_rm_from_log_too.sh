0fab8fac1895f78fa0954c5eb64d21cca784dae0
Toshinori Sato (@overlast) <overlasting@gmail.com>
2015-03-25 20:10:06 +0900

    Add script to delete an obsolete data from log

---
+++ libexec/git_rm_from_log_too.sh
@@ -0,0 +1,28 @@
+ !#/usr/bin/bash
+
+ FILE_PATH=$1
+ ECHO_PREFIX='[git_rm_from_history_too] '
+
+ echo "${ECHO_PREFIX} Start."
+
+ if [ ! -e ${FILE_PATH} ]; then
+     echo "${ECHO_PREFIX} ${FILE_PATH} is not found"
+     exit 1
+ fi
+
+ git pull
+
+ if [ -e ${FILE_PATH} ]; then
+     echo "${ECHO_PREFIX} git rm ${FILE_PATH}"
+     git rm ${FILE_PATH}
+     echo "${ECHO_PREFIX} git commit"
+     git commit -m "Remove obsolete file: ${FILE_PATH}"
+     echo "${ECHO_PREFIX} git push"
+     git push
+     echo "${ECHO_PREFIX} delete file from log"
+     git filter-branch -f --tree-filter "rm -f ${FILE_PATH}"
+     echo "${ECHO_PREFIX} git push --force"
+     git push --force
+ fi
+
+ echo "${ECHO_PREFIX} Finish."
