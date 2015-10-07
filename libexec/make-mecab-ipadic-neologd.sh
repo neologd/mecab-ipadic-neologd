bc39111064f043801e980ab49abece4723af8c65
Toshinori Sato (@overlast) <overlasting@gmail.com>
2015-10-07 14:14:35 +0900

    Add --insecure option

--- libexec/make-mecab-ipadic-neologd.sh
+++ libexec/make-mecab-ipadic-neologd.sh
@@ -50,7 +50,7 @@ ORG_DIC_NAME=mecab-ipadic-2.7.0-20070801
 NEOLOGD_DIC_NAME=mecab-ipadic-2.7.0-20070801-neologd-${YMD}
 
 if [ ! -e ${BASEDIR}/../build/${ORG_DIC_NAME}.tar.gz ]; then
-    STATUS_CODE=`curl -IL https://drive.google.com -s -w '%{http_code}\n' -o /dev/null`
+    STATUS_CODE=`curl --insecure -IL https://drive.google.com -s -w '%{http_code}\n' -o /dev/null`
     if [ ${STATUS_CODE} = 200 ]; then
         IS_NETWORK_ONLINE=1
     else
