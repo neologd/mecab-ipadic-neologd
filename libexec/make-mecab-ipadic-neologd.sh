3fbc2f75dea9e10a2d97bb3b2c73df00f76e552b
Toshinori Sato (@overlast) <overlasting@gmail.com>
2016-05-01 16:38:51 +0900

    Delete echo for debugging

--- libexec/make-mecab-ipadic-neologd.sh
+++ libexec/make-mecab-ipadic-neologd.sh
@@ -272,7 +272,6 @@ else
     echo "${ECHO_PREFIX} We can't intall ${BASEDIR}/../seed/${ADJECTIVE_VERB_SEED_FILE_NAME}"
 fi
 
-echo ${MAX_SURFACE_LEN}
 SEED_FILE_NAMES=($(find ${NEOLOGD_DIC_DIR}/* -name "*.csv"))
 
 if [ ${MIN_SURFACE_LEN} -gt 0 -o ${MAX_SURFACE_LEN} -gt 0 ]; then
