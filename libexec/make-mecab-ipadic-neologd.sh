0580cfae19a77eb609c99aa9fbc779a62d15d005
Matthew Bowen <matthew.bowen1989@protonmail.com>
2023-07-01 17:03:19 +0900

    Trim whitespace from calculated hash (2nd place)

--- libexec/make-mecab-ipadic-neologd.sh
+++ libexec/make-mecab-ipadic-neologd.sh
@@ -108,7 +108,7 @@ else
     echo "$ECHO_PREFIX Original mecab-ipadic file is already there."
 fi
 
-IPADIC_HASH_VAL=`openssl sha1 ${BASEDIR}/../build/${ORG_DIC_NAME}.tar.gz | cut -d $' ' -f 2,2`
+IPADIC_HASH_VAL=`openssl sha1 ${BASEDIR}/../build/${ORG_DIC_NAME}.tar.gz | cut -d $' ' -f 2,2 | xargs`
 if [ "${IPADIC_HASH_VAL}" != "0d9d021853ba4bb4adfa782ea450e55bfe1a229b" ]; then
     echo "$ECHO_PREFIX Fail to download ${BASEDIR}/../build/${ORG_DIC_NAME}.tar.gz"
     echo "$ECHO_PREFIX You should remove ${BASEDIR}/../build/${ORG_DIC_NAME}.tar.gz before retrying to install mecab-ipadic-NEologd"
